ebdfc517153c3d933371c9d02e8af35d08f995e2
nokazn <baleariclemon@gmail.com>
2020-03-13 06:21:35 +0000

    init

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "env": {
+      "es6": true,
+      "node": true
+    },
+    "extends": [
+      "plugin:@typescript-eslint/eslint-recommended",
+      "airbnb-base",
+      "plugin:import/errors",
+      "plugin:import/warnings",
+      "plugin:import/typescript",
+    ],
+    "globals": {
+      "Atomics": "readonly",
+      "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+      "ecmaVersion": 2018,
+      "sourceType": "module"
+    },
+    "plugins": [
+      "@typescript-eslint"
+    ],
+    "rules": {
+      "import/extensions": [2, {
+        "js": "never",
+        "ts": "never",
+        "json": "never"
+      }],
+      "indent": [2, 2],
+      "lines-between-class-members": 0,
+      "no-console": 0,
+      // typescript-eslint の no-unuserd-vars を有効にする
+      "no-unused-vars": 0,
+      "@typescript-eslint/no-unused-vars": 2,
+    }
+  };
